7a9476ce45449f0337d2e7e639c22237d6c31639
Yevgen <107062952+Yevgen1984@users.noreply.github.com>
2022-10-07 16:35:48 +0300

    add hw2 (first part 07.10)

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -1,21 +1,23 @@
 //import PropTypes from 'prop-types';
 //import { Paragraf, Span } from './User.styled';
-//import { countGoodFeedback } from './App'
 
 export const Feedback = ({
   options: { good, neutral, bad },
-  countGoodFeedback,
+  countFeedback,
+  countTotalFeedback,
+  countPositiveFeedbackPercentage,
 }) => {
+  
   return (
     <>
       <h2>Please leave feedback</h2>
-      <button name="good" type="button" onClick={countGoodFeedback}>
+      <button name="good" type="button" onClick={countFeedback}>
         Good
       </button>
-      <button name="neutral" type="button" onClick={countGoodFeedback}>
+      <button name="neutral" type="button" onClick={countFeedback}>
         Neutral
       </button>
-      <button name="bad" type="button" onClick={countGoodFeedback}>
+      <button name="bad" type="button" onClick={countFeedback}>
         Bad
       </button>
       <h2>Statistics</h2>
@@ -23,8 +25,8 @@ export const Feedback = ({
         <li>Good: {good}</li>
         <li>Neutral: {neutral}</li>
         <li>Bad: {bad}</li>
-        <li>Total: {bad}</li>
-        <li>Positive feedback: {bad}%</li>
+        <li>Total: {countTotalFeedback}</li>
+        <li>Positive feedback: {countPositiveFeedbackPercentage()}%</li>
       </ul>
     </>
   );
